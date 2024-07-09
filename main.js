9dd28cbf345fe5333226c3087247f0519fd3622d
Sarawar Hossain <sh-riyad>
2024-07-10 01:44:02 +0600

    theme complete

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-const themes = ['Light', 'Dark', 'Cupcake', 'Bumblebee', 'emerald', 'corporate', 'synthwave', 'Retro', 'Cyberpunk', 'Valentine','Halkoween','Garden','Forest','Aqua','Lofi','Pastel']
+const themes = ['Default','Light', 'Dark', 'Cupcake', 'Bumblebee', 'emerald', 'corporate', 'synthwave', 'Retro', 'Cyberpunk', 'Valentine','Halkoween','Garden','Forest','Aqua','Lofi','Pastel']
 
 
 
@@ -12,27 +12,77 @@ const themeButtonText = document.querySelector("#themeButtonText");
 const modal = document.querySelector("#modal");
 const modalCross = document.querySelector("#modalCross");
 const setApiKeyButton = document.querySelector("#setApiKeyButton");
-const mainContent = document.querySelector("#mainContent");
 const searchInput = document.querySelector("#search"); 
+const container = document.querySelector("#container");
 
 const apiKey = localStorage.getItem("api_key") || "none";
 
+
+
+
+function randerUrl() {
+  const parsedUrl = new URL(window.location.href);
+  const paramTheme = parsedUrl.searchParams.get("theme");
+  if (paramTheme) {
+    localStorage.setItem("theme", paramTheme);
+  }
+
+  randerTheme();
+  
+}
+
+// function randerUrl() {
+//   // console.log(window.location);
+//   // console.log(window.location.search);
+//   const searchString = window.location.search;
+//   const searchParams = new URLSearchParams(searchString);
+//   // console.log(searchParams);
+//   const theme = searchParams.get("theme");
+//   if (theme) {
+//     localStorage.setItem("theme", theme);
+//   } else {
+//     const theme = localStorage.getItem("theme") || "default";
+//     searchParams.append("theme", theme);
+//   }
+//   randerTheme();
+
+// }
+
+function randerTheme() {
+  const currentTheme = localStorage.getItem("theme") || "default";
+  container.className = "";
+  container.classList.add(`theme-${currentTheme.toLowerCase()}`)
+  themeButtonText.textContent = formatButtonText(currentTheme);
+  localStorage.setItem("theme", currentTheme);
+}
+
+function formatButtonText(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
 function showmodal() {
   modal.classList.remove("hidden");
 }
 function hidemodal() {
   modal.classList.add("hidden");
 }
+function hideDropDown() {
+  dropdownBar.classList.add("hidden");
+  dropdownBar.classList.remove("absolute", "right-2");
+}
+function hideThemeOptionsDiv() {
+  themeOptionsDiv.classList.add("hidden");
+}
+
+function keyPress(event) {
+  if (event.key === "Escape") {
+    hidemodal();
+    hideThemeOptionsDiv()
+    hideDropDown()
+  }
+}
 
 document.addEventListener("keydown", (event) => {
-  if (event.key === "Escape") {
-    modal.classList.add("hidden");
-
-    themeOptionsDiv.classList.add("hidden");
-
-    dropdownBar.classList.add("hidden");
-  dropdownBar.classList.remove("absolute", "right-2");
-  }
+  keyPress(event)
 })
 
 searchInput.addEventListener("input", (event) => {
@@ -68,10 +118,12 @@ function renderThemeList() {
 
     const selectedTheme = ThemeButtonDiv.querySelector("#themeButton");
     selectedTheme.addEventListener("click", () => {
-      themeButtonText.textContent = theme;
+      themeButtonText.textContent = formatButtonText(theme);
       localStorage.setItem("theme", theme);
+      randerTheme()
       themeOptionsDiv.classList.add("hidden");
-    })
+      
+   })
     // ThemeButton.innerText = theme;
     // ThemeButton.classList.add("w-full", "text-start","px-3", "py-1", "rounded-md", "font-mono", "text-base", "hover:bg-red-500");
     themeOptionsDiv.append(ThemeButtonDiv);
@@ -93,7 +145,8 @@ dropdown.addEventListener("click", () => {
 
   }
 })
-  
+
+
 themeOptionsButton.addEventListener("click", () => {
   if (themeOptionsDiv.classList.contains("hidden")) {
     themeOptionsDiv.classList.remove("hidden");
@@ -105,4 +158,8 @@ themeOptionsButton.addEventListener("click", () => {
    
 })
 
-renderThemeList()
+window.addEventListener("load", () => {
+  randerUrl();
+  randerTheme();
+  renderThemeList()
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./*js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        customColor: {
+          primary: 'var(--color-primary-text)',
+          secondary: 'var(--color-secondary-text)',
+        },
+      },
+      backgroundColor: {
+        customColor: {
+          body: 'var(--color-body-bg)',
+          section: 'var(--color-section-bg)',
+          button: 'var(--color-button-bg)',
+          'button-hover' : 'var(--color-button-hover)',
+        }
+      }
+    },
   },
   plugins: [],
 }
